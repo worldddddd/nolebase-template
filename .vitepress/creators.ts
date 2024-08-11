6f7f23a1131773473ca42e3c37255da69ca2c878
worldddddd <1748219043@qq.com>
2024-08-12 00:30:21 +0800

    update

--- .vitepress/creators.ts
+++ .vitepress/creators.ts
@@ -19,32 +19,32 @@ export interface Creator {
 const getAvatarUrl = (name: string) => `https://github.com/${name}.png`
 
 export const creators: Creator[] = [
-  {
-    name: '絢香猫',
-    avatar: '',
-    username: 'nekomeowww',
-    title: 'Nólëbase 原始创作者',
-    desc: '开发者，专注于基础设施维护，数据分析，后端、DevOps 开发',
-    links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/nekomeowww' },
-      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/ayakaneko' },
-    ],
-    nameAliases: ['nekomeowww', '绚香猫', '絢香猫', 'Neko Ayaka', 'Ayaka Neko'],
-    emailAliases: ['neko@ayaka.moe'],
-  },
-  {
-    name: '絢香音',
-    avatar: '',
-    username: 'LittleSound',
-    title: 'Nólëbase 原始创作者',
-    desc: '开源开发者，专注于前端，以及前端相关工具库和工具链开发',
-    links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/LittleSound' },
-      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/OikawaRizumu' },
-    ],
-    nameAliases: ['LittleSound', '绚香音', '絢香音', 'Rizumu Oikawa', 'Rizumu Ayaka', 'Ayaka Rizumu', 'Rizumu'],
-    emailAliases: ['rizumu@ayaka.moe', 'rizumu@oqo.moe'],
-  },
+  // {
+  //   name: '絢香猫',
+  //   avatar: '',
+  //   username: 'nekomeowww',
+  //   title: 'Nólëbase 原始创作者',
+  //   desc: '开发者，专注于基础设施维护，数据分析，后端、DevOps 开发',
+  //   links: [
+  //     { type: 'github', icon: 'github', link: 'https://github.com/nekomeowww' },
+  //     { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/ayakaneko' },
+  //   ],
+  //   nameAliases: ['nekomeowww', '绚香猫', '絢香猫', 'Neko Ayaka', 'Ayaka Neko'],
+  //   emailAliases: ['neko@ayaka.moe'],
+  // },
+  // {
+  //   name: '絢香音',
+  //   avatar: '',
+  //   username: 'LittleSound',
+  //   title: 'Nólëbase 原始创作者',
+  //   desc: '开源开发者，专注于前端，以及前端相关工具库和工具链开发',
+  //   links: [
+  //     { type: 'github', icon: 'github', link: 'https://github.com/LittleSound' },
+  //     { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/OikawaRizumu' },
+  //   ],
+  //   nameAliases: ['LittleSound', '绚香音', '絢香音', 'Rizumu Oikawa', 'Rizumu Ayaka', 'Ayaka Rizumu', 'Rizumu'],
+  //   emailAliases: ['rizumu@ayaka.moe', 'rizumu@oqo.moe'],
+  // },
 ].map<Creator>((c) => {
   c.avatar = c.avatar || getAvatarUrl(c.username)
   return c as Creator
